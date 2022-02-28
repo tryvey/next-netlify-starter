8e6507a6f3a892bdad429b46388eb18b8db9bde3
James Treyvaud <tryvey@gmail.com>
2022-02-28 14:31:51 +1100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,13 +6,13 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>jamietreyvaud.com</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
+        <Header title="Under construction" />
+        <p className="Currently updating website content">
           Get started by editing <code>pages/index.js</code>
         </p>
       </main>
