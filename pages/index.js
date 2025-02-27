1efe1b51c9e660448caf4e8a00112fabfdb2d861
Jamie Treyvaud <tryvey@gmail.com>
2025-02-27 22:42:13 +1100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,10 @@ import Footer from '@components/Footer'
 export default function Home() {
   return (
     <div className="container">
-      <Head>
+      //<Head>
         <title>jamietreyvaud.com</title>
         <link rel="icon" href="/favicon.ico" />
-      </Head>
+      //</Head>
 
       <head>
     <meta charset="UTF-8">
