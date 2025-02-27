d1950f65b7f3cdcc3ac1b2b01d4a2a2bb356b566
Jamie Treyvaud <tryvey@gmail.com>
2025-02-28 09:51:07 +1100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="container">
       //<Head>
-      // <title>jamietreyvaud.com</title>
+      //<title>jamietreyvaud.com</title>
       //<link rel="icon" href="/favicon.ico" />
       //</Head>
 
